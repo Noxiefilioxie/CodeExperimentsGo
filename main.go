e5d4432b48b54c3ec600016bdbeb64ec369077d9
jonk <jonk@pop-os.lan>
2025-04-12 01:45:27 +0200

    binarysearch with recursion

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+var array = [5]int{1, 2, 5, 4, 3}
 
 func main() {
-	DoFizzBuzz(15, 3, 5)
+	DoBinarySearchRecursion(2, 1, 3)
+}
+
+func DoBinarySearchRecursion(target int, start int, end int) {
+	if start > end {
+		fmt.Println("Not Found")
+		return
+	}
+
+	middle := end + start/2
+
+	if middle == target {
+		println("Target found")
+	}
+	if array[middle] > target {
+		DoBinarySearchRecursion(target, start, middle-1)
+	}
+	if array[middle] < target {
+		DoBinarySearchRecursion(target, middle+1, end)
+	}
 }
 
 func DoFizzBuzz(n int, f int, b int) {
